31fb9daf676d68c0a002c4f5083bffe93842774c
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-12 12:47:34 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,12 +1,20 @@
 # Setup Database
-git clone --depth 1 --branch v0.16.6 git@github.com:vitrivr/cottontaildb.git
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/cottontaildb.git
+cd ./cottontaildb
+./gradlew build
+cd ..
 
 # Setup Cineast
 git clone --depth 1 --branch p1-demo git@github.com:vitrivr/cineast.git
+cd ./cineast
+./gradlew build
+cd ..
 
 # Setup Engine
 git clone --depth 1 --branch p1-demo git@github.com:vitrivr/vitrivr-engine.git
-
+cd ./vitrivr-engine
+./gradlew build
+cd ..
 
 # Setup FES
 git clone --depth 1 --branch p1-demo git@github.com:vitrivr/feature-extraction-server.git
