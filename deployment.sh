45012ade61100cfec4b3d5624c0509aa230ed3c8
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-12 12:36:37 +0200

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,1 +1,20 @@
-git clone --depth 1 --branch lsc24 git@github.com:vitrivr/vitrivr-engine.git
+# Setup Database
+git clone --depth 1 --branch v0.16.6 git@github.com:vitrivr/cottontaildb.git
+
+# Setup Cineast
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/cineast.git
+
+# Setup Engine
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/vitrivr-engine.git
+
+
+# Setup FES
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/feature-extraction-server.git
+
+# Setup UI 
+# ng
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/vitrivr-ng.git
+
+# ng-min
+git clone --depth 1 --branch p1-demo git@github.com:vitrivr/vitrivr-ng-min.git
+
