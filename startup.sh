4492e795bf517710037876050406b354f28077d8
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-13 17:38:29 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
+cd ~/retrieval-deployments
+
 # Restart nginx
+
+
 sudo pkill -f nginx & wait $!
 sudo nginx  -c /home/dbisadmin/retrieval-deployments/retrieval-deployments/config/nginx/nginx.conf
 
+cd ~/retrieval-deployments
+./cottontaildb/cottontaildb-full/bin/cottontaildb ./retrieval-deployments/config/cottontail/config.json
