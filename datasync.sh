fb0019401d97a5043647271bb4a229cc8b5df349
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-14 11:56:25 +0200

    Update datasync.sh

--- datasync.sh
+++ datasync.sh
@@ -8,8 +8,8 @@
 cd ~/retrevial-deployments
 
 # Restore the schemas
-schema restore lhe --input=./data/p1-data/dump/lhe/lhe.dump
-schema restore v3c --input=./data/p1-data/dump/v3c/v3c.dump
-schema restore mvk --input=./data/p1-data/dump/mvk/mvk.dump
+schema restore lhe --input=./demo/p1-data/dump/lhe/lhe.dump
+schema restore v3c --input=./demo/p1-data/dump/v3c/v3c.dump
+schema restore mvk --input=./demo/p1-data/dump/mvk/mvk.dump
 
 
