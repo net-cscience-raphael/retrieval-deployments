7e74fb678f3a3e02f9b2d0e15c70fae1a074ff7e
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-14 12:22:07 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,18 +4,23 @@ cd ~/retrieval-deployments
 
 # Restart nginx
 
-
 sudo pkill -f nginx & wait $!
 sudo nginx  -c /home/dbisadmin/retrieval-deployments/retrieval-deployments/config/nginx/nginx.conf
 
 
+# Start cotoontail
 cd ~/retrieval-deployments
 ./cottontaildb/cottontaildb-full/bin/cottontaildb ./retrieval-deployments/config/cottontail/config.json
 
 tmux split-window 
 
+# Start carotly
 cd ~/retrieval-deployments
 ./cottontaildb/cottontaildb-full/bin/carrotli
 
 tmux split-window 
 
+# Start cineaast
+
+cd ~/retrieval-deployments
+java -jar ./cineast/cineast-api/build/libs/cineast-api-3.12.4-all.jar ./retrieval-deployments/config/cineast/cineast.json 
