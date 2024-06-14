40813f382b82ad9737c8fd1e9e35ded554fa12d3
Raphael Waltenspül <raphael.waltenspuel@unibas.ch>
2024-06-14 11:54:40 +0200

    Update datasync.sh

--- datasync.sh
+++ datasync.sh
@@ -1,3 +1,15 @@
 
 # Assuming why is the network Y:\
-scp -r  Y:/p1-demo/* dbisadmin@192.43.193.15:~/retrieval-deployments/data/p1-demo
+# Run on Host machine
+# TODO, 
+# scp -r  Y:/p1-demo/* dbisadmin@192.43.193.15:~/retrieval-deployments/data/p1-demo
+
+# Run on node
+cd ~/retrevial-deployments
+
+# Restore the schemas
+schema restore lhe --input=./data/p1-data/dump/lhe/lhe.dump
+schema restore v3c --input=./data/p1-data/dump/v3c/v3c.dump
+schema restore mvk --input=./data/p1-data/dump/mvk/mvk.dump
+
+
